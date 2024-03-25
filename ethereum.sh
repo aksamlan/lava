b0d23980281b74ca830d0e08f36f1ad55d9cc371
Huseyin <154274674+aksamlan@users.noreply.github.com>
2024-03-25 01:08:33 +0100

    Update ethereum.sh

--- ethereum.sh
+++ ethereum.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-RPC_URL="https://eth1.lava.build/lava-referer-8b51600b-b3cc-4893-89ec-5de41f0724db/"
+RPC_URL="https://eth1.lava.build/lava-referer-5f7d30e8-9f09-4fda-9baa-ad5af938b2fe/"
 
 OUTPUT_DIR="logs"
 
